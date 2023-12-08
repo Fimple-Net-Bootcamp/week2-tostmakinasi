c91619799101a89e5c51232edd2ad55e5ab8c872
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-12-08 16:28:40 +0300

    Changed QueryParameters Struct to Record Class

--- SpaceWeatherAPI/CustomQueryParameters/QueryParameters.cs
+++ SpaceWeatherAPI/CustomQueryParameters/QueryParameters.cs
@@ -3,74 +3,14 @@ using System.ComponentModel;
 
 namespace SpaceWeatherAPI.CustomQueryParameters
 {
-    public struct QueryParameters
+    /// <param name="SearchTerm"> Sets the search term for filtering results.
+    /// **Filter On Columns:** Name, Condition </param>
+    /// <param name="Sorting"> Sets the sorting column and order.**Order Types:** "asc", "desc" **For Example:** "name_asc, id_desc,condition"</param>
+    /// <param name="PageNumber"> Sets the page number. </param>
+    /// <param name="PageSize"> Sets the page size. </param>
+    public record class QueryParameters(string SearchTerm = "", string Sorting ="", int PageNumber = 1, int PageSize = 10)
     {
-        public QueryParameters()
-        {
-            SearchTerm = "";
-            PageNumber = 1;
-            PageSize = 10;
-        }
-        /// <summary>
-        /// Sets the search term for filtering results.
-        /// **Filter On Columns:** Name, WeatherInfo.Condition
-        /// </summary>
-        /// <remarks>For example: "Gezegen" or "Blizzard"</remarks>
-        public string SearchTerm { get; set; }
 
-        private string _column = "id";
-
-        private string _order = "asc";
-
-        /// <summary>
-        /// Sets the sorting column and order.
-        /// **Example:** "name_asc"
-        /// </summary>
-        /// <remarks>For example: "name_asc"</remarks>
-        public string Sort
-        {
-            get
-            {
-                return $"{_column}_{_order}";
-            }
-            set
-            {
-                var parameter = value.Split('_');
-                _column = parameter[0];
-                if(parameter.Length > 1)
-                {
-                    _order = parameter[1];
-                }
-            }
-        }
-
-    
-        public string GetColumn()
-        {
-            return _column;
-        }
-
-  
-        public string GetOrder()
-        {
-            return _order;
-        }
-
-        /// <summary>
-        /// Sets the page number.
-        /// </summary>
-        public int PageNumber { get; set; }
-
-        /// <summary>
-        /// Sets the page size.
-        /// </summary>
-        public int PageSize { get; set; }
-
-        public void ValidationPageParams()
-        {
-            this.PageNumber = PageNumber < 1 ? 1 : PageNumber;
-            this.PageSize = PageSize < 1 ? 5 : PageSize;
-        }
-
+        
     }
 }
